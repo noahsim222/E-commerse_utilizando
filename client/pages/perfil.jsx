13fa4bdd8789a25ea5ff7361099002fa781e7008
noahsim222 <noahsim222@gmail.com>
2023-05-15 22:30:28 -0300

    mejorar responsive

--- client/pages/perfil.jsx
+++ client/pages/perfil.jsx
@@ -26,12 +26,12 @@ const perfil = () => {
                             <img class="mb-20 max-w-auto m-10 w-80 mr-20 h-64 rounded-lg " src={usuario.foto} alt="" />
                             <div class="  text-center  p-4 w-98 rounded-lg  ">
                                 <div className='mr-80 p-4'>
-                                    <p class="text-3xl flex gap-5 text-black font-medium mb-20">
+                                <p class="text-3xl flex gap-5 text-sky-900 font-medium mb-20">
                                         Bienvenido {usuario.nombre}!</p>
-                                    <p class="text-3xl flex gap-5 text-black font-medium mb-20">
+                                <p class="text-3xl flex gap-5 text-sky-900 font-medium mb-20">
                                         <b>Ubicacion: </b>{usuario.ubicacion}
                                     </p>
-                                    <p class="text-3xl text-black flex gap-5 font-medium mb-20">
+                                    <p class="text-3xl text-sky-900 flex gap-5 font-medium mb-20">
                                         <b>Edad: </b> {usuario.edad}
                                     </p>
                                 </div>
